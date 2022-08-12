b62b3dda5a9d86cafdbdf30ccd3f68d358bc9125
marcus-hugo <78324205+marcus-hugo@users.noreply.github.com>
2022-08-12 09:53:11 -0500

    set local storage for dark mode

--- app.js
+++ app.js
@@ -3,10 +3,42 @@ const body = document.querySelector('body')
 const toggleBtn = document.querySelector('.button-toggle--theme-light')
 const allBtns = document.querySelectorAll('.button')
 const allPs = document.querySelectorAll('p')
+let darkMode = localStorage.getItem('darkMode')
+
+if (darkMode === 'false') {
+    
+    toggleBtn.classList.contains('light') ? 
+    toggleBtn.innerText = 'Light' : 
+    toggleBtn.innerText = "Dark"
+
+    body.classList.add('light')
+    for (let b of allBtns) {
+        b.classList.add('light')
+    }
+
+    for (let p of allPs) {
+        p.classList.add('light')
+    }
+
+   
+} else {
+
+    body.classList.remove('light')
+    for (let b of allBtns) {
+        b.classList.remove('light')
+    }
+
+    for (let p of allPs) {
+        p.classList.remove('light')
+    }
+
+  
+}
 
 toggleBtn.addEventListener('click', () => {
     
     body.classList.toggle('light')
+
     toggleBtn.classList.contains('light') ? 
     toggleBtn.innerText = 'Light' : 
     toggleBtn.innerText = "Dark"
@@ -17,5 +49,11 @@ toggleBtn.addEventListener('click', () => {
 
     for (let p of allPs) {
         p.classList.toggle('light')
+    }
+
+    if (body.classList.contains('light')) {
+        localStorage.setItem('darkMode', 'false')
+    } else {
+        localStorage.setItem('darkMode', 'true')
     }
 })
