0090564691e1c492e74c1c0905673a146e4e1451
marcushugo <phantomboss300@yahoo.com>
2023-02-11 18:40:45 -0600

    updated theme, added mobile menu

---
+++ js/themeToggle.js
@@ -0,0 +1,84 @@
+const body = document.querySelector("body")
+const nav = document.querySelector(".nav")
+const projects = document.querySelectorAll(".project")
+const checkbox = document.getElementById("theme-toggle")
+const toggle = document.querySelector(".toggle-switch__input")
+var mediaQueryList = window.matchMedia("(prefers-color-scheme: light)")
+let isToggleChecked = toggle.getAttribute("aria-checked")
+let darkMode = localStorage.getItem("darkMode")
+
+function addLightTheme() {
+  body.classList.remove("dark-theme")
+  body.classList.add("light-theme")
+
+  nav.classList.remove("dark-theme")
+  nav.classList.add("light-theme")
+
+  for (let i = 0; i < projects.length; i++) {
+    projects[i].classList.remove("dark-theme")
+    projects[i].classList.add("light-theme")
+  }
+}
+
+function addDarkTheme() {
+  body.classList.remove("light-theme")
+  body.classList.add("dark-theme")
+
+  nav.classList.remove("light-theme")
+  nav.classList.add("dark-theme")
+
+  for (let i = 0; i < projects.length; i++) {
+    projects[i].classList.remove("light-theme")
+    projects[i].classList.add("dark-theme")
+  }
+}
+
+function systemChange() {
+  console.log("change event listener added")
+
+  if (mediaQueryList.matches === false) {
+    toggle.setAttribute("aria-checked", "true")
+    checkbox.checked = true
+    addDarkTheme()
+  } else {
+    toggle.setAttribute("aria-checked", "false")
+    checkbox.checked = false
+    addLightTheme()
+  }
+}
+
+mediaQueryList.addEventListener("change", systemChange)
+
+toggle.addEventListener("click", function () {
+  if (checkbox.checked === false) {
+    localStorage.setItem("darkMode", "false")
+    toggle.setAttribute("aria-checked", "false")
+    addLightTheme()
+  } else if (checkbox.checked === true) {
+    localStorage.setItem("darkMode", "true")
+    toggle.setAttribute("aria-checked", "true")
+    addDarkTheme()
+  }
+})
+
+window.onload = () => {
+  if (mediaQueryList.matches === false) {
+    toggle.setAttribute("aria-checked", "true")
+    checkbox.checked = true
+  } else {
+    toggle.setAttribute("aria-checked", "false")
+    checkbox.checked = false
+  }
+
+  if (darkMode === "true") {
+    mediaQueryList.removeEventListener("change", systemChange)
+    toggle.setAttribute("aria-checked", "true")
+    checkbox.checked = true
+    addDarkTheme()
+  } else if (darkMode === "false") {
+    mediaQueryList.removeEventListener("change", systemChange)
+    toggle.setAttribute("aria-checked", "false")
+    checkbox.checked = false
+    addLightTheme()
+  }
+}
